2d1df29a61daa03aafba2dc154616973f49676ce
Buchkiman <irakligamer8@mail.ru>
2025-01-23 21:03:54 +0400

    .
    .

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float speed = 9.5f;
+    [SerializeField] Camera camera;
+
+    void Update()
+    {
+        Move();
+        Turn();
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Shoot");
+        }
+    }
+
+    void Turn()
+    {
+        Vector2 mouseWorldPosition = camera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 lookDirection = mouseWorldPosition - (Vector2)transform.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    void Move()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        transform.position = transform.position + new Vector3(horizontal, vertical) * Time.deltaTime * speed;
+    }
+}
