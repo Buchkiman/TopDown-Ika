0947194abf3c6e697539625876c4c25daae48ad5
Buchkiman <irakligamer8@mail.ru>
2025-01-27 19:43:35 +0400

    .
    .

--- Assets/Scripts/info.cs
+++ Assets/Scripts/info.cs
@@ -4,27 +4,58 @@ using UnityEngine;
 
 public class info : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    string FirstName = "Irakli";
+    string LastName = "Patsia";
+    int MyAge = 18;
+    string MyHobby = "playing video games";
 
-    // Update is called once per frame
+
+
     void Update()
     {
-        string FirstName = "Irakli";
-        string LastName = "Patsia";
-        int MyAge = 18;
-        string MyHobby = "playing video games";
-        string GetFullName = (FirstName + " " +  LastName);
-        string GetFullBio = ("My name is" + " " + FirstName + "I am" + " " + MyAge + " " + "years old" + " " + "I love" + " " + MyHobby);
+
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if (vertical == 1)
+        {
+            string fullbio = GetFullBio(FirstName, LastName, MyAge, MyHobby);
+            Debug.Log(fullbio);
+
+        }
 
 
-        float Horizontal = Input.GetAxisRaw("horizontal");
+        float horizontal = Input.GetAxisRaw("Horizontal");
 
-        float Vertical = Input.GetAxisRaw("vertical");
+        if (horizontal == 1)
+        {
+            string fullname = GetFullName(FirstName, LastName);
+            Debug.Log(fullname);
+
+
+        }
 
 
     }
+
+    string GetFullBio(string firstname, string lastname, int myage, string myhobby)
+    {
+        string fullbio = firstname + " " + lastname + " " + myage + " " + myhobby;
+        return fullbio;
+
+    }
+
+    string GetFullName(string firstname, string lastname) 
+    {
+        string fullname = firstname + " " + lastname;
+        return fullname;
+
+
+
+    }
+
+
+
+
+
+
 }
