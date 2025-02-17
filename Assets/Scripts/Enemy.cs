6d322c8d29847c13c30777aa93aded547683ce1b
Buchkiman <irakligamer8@mail.ru>
2025-02-17 19:10:07 +0400

    .
    .

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,4 +5,12 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] int hitCount = 3;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+
+
+
+        //Destroy(gameObject);
+        //Destroy(col.gameObject);
+    }
 }
