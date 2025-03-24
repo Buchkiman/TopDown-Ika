7e741035684fdd6e293dc12e250e99e9e6a0272a
Buchkiman <irakligamer8@mail.ru>
2025-03-24 20:19:55 +0400

    .
    .

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
 
+    [SerializeField] GameObject enemyPrefab;
     float spawnRate = 2.5f;
 
     void Update()
@@ -13,7 +14,7 @@ public class EnemySpawner : MonoBehaviour
 
         if (spawnRate <= 0)
         {
-            Debug.Log("enemy spawned");
+            Instantiate(enemyPrefab, transform.position, transform.rotation);
             spawnRate = 2.5f;
         }
 
